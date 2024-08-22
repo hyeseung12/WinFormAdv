748082ea6811f0b7b87bdc8e8288edebb96bda6a
hyeseung12 <peace10200khs@gmail.com>
2024-08-22 11:41:44 +0900

    상태바 작성

--- WinFormAdv/Form1.cs
+++ WinFormAdv/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormAdv
         {
             InitializeComponent();
         }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
