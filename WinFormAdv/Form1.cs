0347010e4f7a5f1d1cd6df9d6ea23b8567ef866e
hyeseung12 <peace10200khs@gmail.com>
2024-08-22 11:48:44 +0900

    도움말 > 도움말 보기 구현

--- WinFormAdv/Form1.cs
+++ WinFormAdv/Form1.cs
@@ -26,5 +26,10 @@ namespace WinFormAdv
         {
             Application.Exit();
         }
+
+        private void 도움말보기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://support.microsoft.com/ko-kr/windows/windows%EC%97%90%EC%84%9C-%EB%8F%84%EC%9B%80%EB%A7%90%EC%9D%84-%EB%B3%B4%EB%8A%94-%EB%B0%A9%EB%B2%95-711b6492-0435-0038-8706-7c6b0feb200a");
+        }
     }
 }
